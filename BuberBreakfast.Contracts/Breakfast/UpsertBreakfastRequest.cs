58b7664380002233b60bd025d029ce4c3bff5b39
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-13 22:04:37 +0100

    All in one solution now

---
+++ BuberBreakfast.Contracts/Breakfast/UpsertBreakfastRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Upsert is IF exists then update, else insert new row with data
+namespace BuberBreakfast.Contracts.Breakfast
+{
+    public record UpsertBreakfastRequest(
+        string Name,
+        string Description,
+        DateTime StartDateTime,
+        DateTime EndDateTime,
+        List<string> Savoury,
+        List<string> Sweet
+        );
+}
