6ae2b20194f34216237e53a8d36f5d467961d140
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-13 22:14:12 +0100

    WIP - nos da

--- BuberBreakfast/Program.cs
+++ BuberBreakfast/Program.cs
@@ -1,8 +1,12 @@
-﻿var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-
+﻿//Builder variable can be uses for dependency injection
+var builder = WebApplication.CreateBuilder(args);
+//Create scope and organise it this way
+{
+    builder.Services.AddControllers();
+}
 var app = builder.Build();
-
-app.UseHttpsRedirection();
-app.MapControllers();
-app.Run();
+{
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.Run();
+}
